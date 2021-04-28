3af2f127f5c093aa3c3d4f93bc211d5e1702c82d
Benjamin Arrazate <benjamin.arrazate@icloud.com>
2021-04-28 17:26:48 -0600

    Inicio de Stack

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace STACK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string opcion = "";
+            while (opcion != "9")
+            {
+
+                opcion = "";
+                while (opcion != "1" && opcion != "2" && opcion != "9")
+                {   
+                    Console.WriteLine("1) Decir \"Hola Mundo!\"");
+                    Console.WriteLine("2) Decir \"Hola [nombre]!\"");
+                    Console.WriteLine("9) Decir Salir");
+
+                    Console.WriteLine("Por favor selecciona una opción:");
+                    opcion = Console.ReadLine();
+
+                    if (opcion != "1" && opcion != "2" && opcion != "9")
+                    {
+                        Console.WriteLine("La opción seleccionada no es correcta");
+                    }
+                }
+
+
+                if (opcion == "1")
+                {
+                    Console.WriteLine("Hola Mundo!");
+
+                }
+                else if (opcion == "2")
+                {
+                    Console.WriteLine("Introduce tu Nombre:");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Hola " + nombre + "!");
+                }
+                else if (opcion == "9")
+                {
+                    Console.WriteLine("Hasta Luego");
+                }
+
+            }
+        }
+
+    }
+
+}
